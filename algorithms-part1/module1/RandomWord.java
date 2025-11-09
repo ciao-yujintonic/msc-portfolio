2003dfb4be877a27a5f350bb780729c340d625e6
ciao-yujintonic <yujin.hong420@gmail.com>
2025-11-09 17:24:16 +0900

    Add main method to RandomWord class for selecting a random word from input

--- algorithms-part1/module1/RandomWord.java
+++ algorithms-part1/module1/RandomWord.java
@@ -3,5 +3,17 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
-    
+    public static void main(String[] args) {
+        int index = 0;
+        String word;
+        String champion = "";
+        while (!StdIn.isEmpty()) {
+            index++;
+            word = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / index)) {
+                champion = word;
+            }
+        }
+        StdOut.println(champion);
+    }
 }
